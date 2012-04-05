9a13239ba0124fa3754ffc33da4d7a18c78f014e
Marijke Habermann <marijke.habermann@gi.alaska.edu>
2012-04-05 10:23:37 -0800

    Separated vel_mosaic2netcdf function from rest of code so that it can be used for non-NSIDC data files.

--- MEaSUREs_Greenland_Ice_Velocity/preprocess.py
+++ MEaSUREs_Greenland_Ice_Velocity/preprocess.py
@@ -6,31 +6,7 @@ import numpy as np
 import os
 import optparse
 
-## Set up the option parser
-parser = optparse.OptionParser()
-parser.usage = """usage: %prog [options] start_year
-Here start_year is one of '2000', '2005', '2006'."""
-parser.description = "This script downloads and preprocesses MEaSUREs Greenland Ice Velocity data"
-
-parser.add_option("-o","--output_directory", dest="output_dir", help="Output directory (needs a ", default="")
-parser.add_option("--add_mask", action="store_true", dest="add_mask", help="Add a mask showing where observations are available")
-
-(options, args) = parser.parse_args()
-output_dir = options.output_dir
-
-if len(args) == 1 and args[0] in ["2000", "2005", "2006"]:
-    start_year = int(args[0])
-else:
-    parser.print_help()
-    exit(0)
-
-years = "%d_%d" % (start_year, start_year + 1)
-ex_filename  = "greenland_vel_mosaic500_%s.ex" % years
-ey_filename  = "greenland_vel_mosaic500_%s.ey" % years
-vx_filename  = "greenland_vel_mosaic500_%s.vx" % years
-vy_filename  = "greenland_vel_mosaic500_%s.vy" % years
-ftp_url = "ftp://anonymous@sidads.colorado.edu/pub/DATASETS/nsidc0478_MEASURES_greenland_V01/%d/" % start_year
-
+# Function that downloads and unpacks NSIDC velocity maps
 def download_and_unpack(url, filename, output_dir):
     if output_dir == "":
         full_filename = filename
@@ -50,84 +26,114 @@ def download_and_unpack(url, filename, output_dir):
         print "Unpacking %s..." % full_filename
         subprocess.call(["gunzip", full_filename + ".gz"])
 
-download_and_unpack(ftp_url, vx_filename, output_dir)
-download_and_unpack(ftp_url, vx_filename + ".geodat", output_dir)
-download_and_unpack(ftp_url, vy_filename, output_dir)
-download_and_unpack(ftp_url, ex_filename, output_dir)
-download_and_unpack(ftp_url, ey_filename, output_dir)
+# Get Joughins vel_mosaic files into PISM usable netcdf format
+def vel_mosaic2netcdf(filename_base, output_filename):
+    files = []
+    for (short_name, long_name) in [("vx", "ice surface velocity in the X direction"),
+                                    ("vy", "ice surface velocity in the Y direction"),
+                                    ("ex",
+                                     "error estimates for the X-component of the ice surface velocity"),
+                                    ("ey",
+                                     "error estimates for the Y-component of the ice surface velocity"),
+                                    ]:
 
-if not output_dir == "":
-    ex_filename  = "%sgreenland_vel_mosaic500_%s.ex" % (output_dir,years)
-    ey_filename  = "%sgreenland_vel_mosaic500_%s.ey" % (output_dir,years)
-    vx_filename  = "%sgreenland_vel_mosaic500_%s.vx" % (output_dir,years)
-    vy_filename  = "%sgreenland_vel_mosaic500_%s.vy" % (output_dir,years)
+        try:
+            os.stat(filename_base + "." + short_name)
+            entry = ("%s.%s" %(filename_base,short_name), short_name, long_name)
+            files.append(entry)
+        except:
+            print "Missing %s.%s. If this file ends with ex or ey the script will work just fine" %(filename_base, short_name)
 
+    grid = np.loadtxt(filename_base + ".vx.geodat", skiprows=1, comments="&")
+    
+    shape = (int(grid[0,1]), int(grid[0,0]))
+    x0 = grid[2,0] * 1e3
+    y0 = grid[2,1] * 1e3
+    dx = grid[1,0]
+    dy = grid[1,1]
+    x1 = x0 + (shape[1] - 1) * dx
+    y1 = y0 + (shape[0] - 1) * dx
+    x = np.linspace(x0, x1, shape[1])
+    y = np.linspace(y0, y1, shape[0])
+    
+    nc = NC("%s.nc" % output_filename, 'w')
+    nc.create_dimensions(x, y)
+    
+    for (filename, short_name, long_name) in files:
+        
+        nc.define_2d_field(short_name, time_dependent = False, nc_type='f4',
+                           attrs = {"long_name"   : long_name,
+                                    "comment"     : "Downloaded from %s" % (ftp_url + filename),
+                                    "units"       : "m / year",
+                                    "mapping"     : "mapping",
+                                    "_FillValue"  : -2e+9})
+        
+        var = np.fromfile(filename, dtype=">f4", count=-1)
+        
+        nc.write_2d_field(short_name, var)
+        
+    # Add a mask that shows where observations are present 
+    if options.add_mask == True:
+        nc.define_2d_field("vel_surface_mask", time_dependent = False, nc_type='i',
+                           attrs = {"long_name"   : "Mask observations; 1 where surface vel. available",
+                                    "comment"     : "Used as vel_misfit_weight in inversion for tauc",
+                                    "units"       : "",
+                                    "mapping"     : "mapping",
+                                    "_FillValue"  : 0})
+        var = np.fromfile(filename_base + ".vx", dtype=">f4", count=-1)
+        mask = var/var
+        mask[var == -2e9] = 0.
+        nc.write_2d_field("vel_surface_mask", mask)
 
-grid = np.loadtxt(vx_filename + ".geodat", skiprows=1, comments="&")
+    mapping = nc.createVariable("mapping", 'c')
+    mapping.grid_mapping_name = "polar_stereographic"
+    mapping.standard_parallel = 70.0
+    mapping.latitude_of_projection_origin = 90.0
+    mapping.straight_vertical_longitude_from_pole = -45.0
+    mapping.ellipsoid = "WGS84"
+    
+    nc.Conventions = "CF-1.4"
+    nc.projection = "+proj=stere +ellps=WGS84 +datum=WGS84 +lon_0=-45 +lat_0=90 +lat_ts=70 +units=m"
+    nc.reference  = "Joughin, I., B. Smith, I. Howat, and T. Scambos. 2010. MEaSUREs Greenland Ice Velocity Map from InSAR Data. Boulder, Colorado, USA: National Snow and Ice Data Center. Digital media."
+    nc.title      = "MEaSUREs Greenland Ice Velocity Map from InSAR Data"
+    
+    from time import asctime
+    import sys
+    separator = ' '
+    historystr = "%s: %s\n" % (asctime(), separator.join(sys.argv))
+    nc.history = historystr
+    
+    nc.close()
 
-shape = (int(grid[0,1]), int(grid[0,0]))
-x0 = grid[2,0] * 1e3
-y0 = grid[2,1] * 1e3
-dx = grid[1,0]
-dy = grid[1,1]
-x1 = x0 + (shape[1] - 1) * dx
-y1 = y0 + (shape[0] - 1) * dx
-x = np.linspace(x0, x1, shape[1])
-y = np.linspace(y0, y1, shape[0])
+## Main code starts here
+if __name__ == "__main__":
+    ## Set up the option parser
+    parser = optparse.OptionParser()
+    parser.usage = """usage: %prog [options] start_year
+    Here start_year is one of '2000', '2005', '2006'."""
+    parser.description = "This script downloads and preprocesses MEaSUREs Greenland Ice Velocity data"
 
-nc = NC("%sMEASUREs_Greenland_%s.nc" % (output_dir,years), 'w')
-nc.create_dimensions(x, y)
+    parser.add_option("-o","--output_directory", dest="output_dir", help="Output directory (needs a ", default="")
+    parser.add_option("--add_mask", action="store_true", dest="add_mask", help="Add a mask showing where observations are available")
+    
+    (options, args) = parser.parse_args()
+    output_dir = options.output_dir
+    
+    if len(args) == 1 and args[0] in ["2000", "2005", "2006"]:
+        start_year = int(args[0])
+    else:
+        parser.print_help()
+        exit(0)
+        
+    years = "%d_%d" % (start_year, start_year + 1)
+    filename = "greenland_vel_mosaic500_%s" % years
+    ftp_url = "ftp://anonymous@sidads.colorado.edu/pub/DATASETS/nsidc0478_MEASURES_greenland_V01/%d/" % start_year
 
-for (filename, short_name, long_name) in [(vx_filename, "vx", "ice surface velocity in the X direction"),
-                                          (vy_filename, "vy", "ice surface velocity in the Y direction"),
-                                          (ex_filename, "ex",
-                                           "error estimates for the X-component of the ice surface velocity"),
-                                          (ey_filename, "ey",
-                                           "error estimates for the Y-component of the ice surface velocity"),
-                                          ]:
+    for ending in [".vx",".vy",".ex",".ey"]:
+        download_and_unpack(ftp_url, filename + ending, output_dir)
 
-    nc.define_2d_field(short_name, time_dependent = False, nc_type='f4',
-                       attrs = {"long_name"   : long_name,
-                                "comment"     : "Downloaded from %s" % (ftp_url + filename),
-                                "units"       : "m / year",
-                                "mapping"     : "mapping",
-                                "_FillValue"  : -2e+9})
+    download_and_unpack(ftp_url, filename + ".vx.geodat", output_dir)
 
-    var = np.fromfile(filename, dtype=">f4", count=-1)
-
-    nc.write_2d_field(short_name, var)
-
-# Add a mask that shows where observations are present 
-if options.add_mask == True:
-    nc.define_2d_field("vel_surface_mask", time_dependent = False, nc_type='i',
-                       attrs = {"long_name"   : "Mask observations; 1 where surface vel. available",
-                                "comment"     : "Used as vel_misfit_weight in inversion for tauc",
-                                "units"       : "",
-                                "mapping"     : "mapping",
-                                "_FillValue"  : 0})
-    var = np.fromfile(vx_filename, dtype=">f4", count=-1)
-    mask = var/var
-    mask[var == -2e9] = 0.
-    nc.write_2d_field("vel_surface_mask", mask)
-
-mapping = nc.createVariable("mapping", 'c')
-mapping.grid_mapping_name = "polar_stereographic"
-mapping.standard_parallel = 70.0
-mapping.latitude_of_projection_origin = 90.0
-mapping.straight_vertical_longitude_from_pole = -45.0
-mapping.ellipsoid = "WGS84"
-
-nc.Conventions = "CF-1.4"
-nc.projection = "+proj=stere +ellps=WGS84 +datum=WGS84 +lon_0=-45 +lat_0=90 +lat_ts=70 +units=m"
-nc.reference  = "Joughin, I., B. Smith, I. Howat, and T. Scambos. 2010. MEaSUREs Greenland Ice Velocity Map from InSAR Data. Boulder, Colorado, USA: National Snow and Ice Data Center. Digital media."
-nc.title      = "MEaSUREs Greenland Ice Velocity Map from InSAR Data"
-
-from time import asctime
-import sys
-separator = ' '
-historystr = "%s: %s\n" % (asctime(), separator.join(sys.argv))
-nc.history = historystr
-
-nc.close()
-
-
+    output_filename = "%sMEASUREs_Greenland_%s" %(output_dir,years)
+    filename_base = "%sgreenland_vel_mosaic500_%s" % (output_dir,years)
+    vel_mosaic2netcdf(filename_base, output_filename)
